437959209391db52c0645989b73c7686c3d86ff2
reneeotten <reneeotten@users.noreply.github.com>
2016-11-28 14:12:09 -0500

    ENH: keep logic for CEST reference point(s) at one place
    Reference data points are assigned in the CESTProfile class; use this
    information (i.e., CESTProfile.reference) when we need it later on.

--- chemex/experiments/cest/plotting.py
+++ chemex/experiments/cest/plotting.py
@@ -47,8 +47,8 @@ def compute_profiles(data_grouped, params):
     profiles = {}
 
     for peak, profile in data_grouped.items():
-        mask = (profile.b1_offsets > -10000.0) & (profile.b1_offsets < 10000.0)
-        mask_ref = np.logical_not(mask)
+        mask_ref = profile.reference
+        mask = np.logical_not(mask_ref)
 
         val_ref = np.mean(profile.val[mask_ref])
 
