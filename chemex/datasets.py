BUG: Correct the boootstrap calculation
The caching of the unscaled profile calculation was messing up with
the creation of a bootstrap profile.

chemex/datasets.py
@@ -16,52 +16,52 @@ class DataSet:
     """DataSet class for handling experimental data."""
 
     def __init__(self, other=None):
-        self.data = []
+        self.dataset = []
         self.chisq_ref = 1e32
 
         if isinstance(other, DataSet):
-            self.data = copy.deepcopy(other.data)
+            self.dataset = copy.deepcopy(other.dataset)
 
         elif isinstance(other, base_profile.BaseProfile):
-            self.data.append(other)
+            self.dataset.append(other)
 
     def __len__(self):
-        return len(self.data)
+        return len(self.dataset)
 
     def __getitem__(self, key):
-        return self.data[key]
+        return self.dataset[key]
 
     def __iter__(self):
-        for some_data in self.data:
+        for some_data in self.dataset:
             yield some_data
 
     def __add__(self, other):
         data_sum = DataSet(self)
-        data_sum.data.extend(other.data)
+        data_sum.dataset.extend(other.data)
         return data_sum
 
     def __radd__(self, other):
         return self.__add__(other)
 
     def __iadd__(self, other):
-        self.data.extend(other.data)
+        self.dataset.extend(other.data)
         return self
 
     @property
     def ndata(self):
-        return sum(len(profile) for profile in self.data)
+        return sum(len(profile) for profile in self.dataset)
 
     def append(self, profile):
         """Append data to an exisiting dataset."""
         if not isinstance(profile, base_profile.BaseProfile):
             raise TypeError
-        self.data.append(profile)
+        self.dataset.append(profile)
 
     def calculate_residuals(self, params, verbose=True, threshold=1e-3):
         """Calculate the residuals."""
 
         residuals = np.concatenate(
-            [profile.calculate_residuals(params) for profile in self.data]
+            [profile.calculate_residuals(params) for profile in self.dataset]
         )
 
         if verbose:
@@ -83,7 +83,7 @@ class DataSet:
         """Write experimental and fitted profiles to a file."""
         datasets = dict()
 
-        for profile in self.data:
+        for profile in self.dataset:
             experiment_name = profile.experiment_name
             datasets.setdefault(experiment_name, list()).append(profile)
 
@@ -167,7 +167,7 @@ class DataSet:
             path, filenames, details, model, included, excluded
         )
 
-        self.data.extend(profiles)
+        self.dataset.extend(profiles)
 
         print("{:<25s} {:<25d}".format(experiment_type, len(profiles)))
 
@@ -178,7 +178,7 @@ class DataSet:
 
         data_bs = DataSet()
 
-        for profile in self.data:
+        for profile in self.dataset:
             data_bs.append(profile.make_bs_profile())
 
         return data_bs
@@ -188,7 +188,7 @@ class DataSet:
 
         data_mc = DataSet()
 
-        for profile in self.data:
+        for profile in self.dataset:
             data_mc.append(profile.make_mc_profile(params=params))
 
         return data_mc
@@ -219,7 +219,7 @@ def read_data(args):
             filename_ = pathlib.Path(filename)
             data.add_dataset_from_file(filename_, model, res_incl, res_excl)
 
-    if not data.data:
+    if not data.dataset:
         sys.exit("\nNo data to fit!\n")
 
     return data

chemex/experiments/base_profile.py
@@ -1,5 +1,6 @@
 """TODO: module docstring."""
 import abc
+import copy
 from functools import lru_cache
 
 import numpy as np
@@ -47,7 +48,9 @@ class BaseProfile(metaclass=abc.ABCMeta):
         self.map_names = {}
         self.basis = None
 
-        self.calculate_unscaled_profile = lru_cache(8)(self.calculate_unscaled_profile)
+        self.calculate_unscaled_profile = lru_cache(256)(
+            self._calculate_unscaled_profile
+        )
 
     def __len__(self):
         return self.data.size
@@ -61,7 +64,7 @@ class BaseProfile(metaclass=abc.ABCMeta):
 
         return residuals[self.mask]
 
-    def calculate_unscaled_profile(self, params_local, **kwargs):
+    def _calculate_unscaled_profile(self, params_local, **kwargs):
         """Calculate the unscaled CEST profile."""
         pass
 
@@ -82,7 +85,7 @@ class BaseProfile(metaclass=abc.ABCMeta):
             scale = 0.0
 
         if kwargs:
-            values = self.calculate_unscaled_profile.__wrapped__(params_local, **kwargs)
+            values = self._calculate_unscaled_profile(params_local, **kwargs)
 
         return values * scale
 
@@ -95,6 +98,17 @@ class BaseProfile(metaclass=abc.ABCMeta):
     def filter_points(self, params=None):
         """TODO: method docstring."""
         pass
+
+    def make_mc_profile(self, params):
+        """Make a profile for MC analysis."""
+
+        profile = copy.copy(self)
+        profile.data["intensities"] = (
+            self.calculate_profile(params)
+            + np.random.randn(len(self.data["intensities"])) * self.data["errors"]
+        )
+
+        return profile
 
     @staticmethod
     def check_exp_details(exp_details=None, expected=None):

chemex/experiments/cest/cest_profile.py
@@ -1,6 +1,5 @@
 """The cest_profile module contains the code for handling CEST profiles."""
 import copy
-from functools import lru_cache
 
 import numpy as np
 from scipy import interpolate
@@ -20,7 +19,10 @@ class CESTProfile(base_profile.BaseProfile):
 
         self.plot_data = plotting.plot_data
         self.mask = np.ones_like(self.data["offsets"], dtype=np.bool)
-        self.reference = np.abs(self.data["offsets"]) >= 1.0e04
+
+    @property
+    def reference(self):
+        return np.abs(self.data["offsets"]) >= 1.0e04
 
     def print_profile(self, params=None):
         """Print the CEST profile."""
@@ -62,17 +64,6 @@ class CESTProfile(base_profile.BaseProfile):
 
         return "\n".join(output).upper()
 
-    def make_mc_profile(self, params):
-        """Make a CEST profile for boostrap analysis."""
-
-        profile = copy.copy(self)
-        profile.data["intensities"] = (
-            self.calculate_profile(params)
-            + np.random.randn(len(self.data["intensities"])) * self.data["errors"]
-        )
-
-        return profile
-
     def make_bs_profile(self):
         """Make a CEST profile for boostrap analysis."""
         indexes = np.array(range(len(self.data["intensities"])))
@@ -87,13 +78,9 @@ class CESTProfile(base_profile.BaseProfile):
         bs_indexes = sorted(bs_indexes)
 
         profile = copy.copy(self)
-        profile.data["offsets"] = profile.data["offsets"][bs_indexes]
-        profile.data["intensities"] = profile.data["intensities"][bs_indexes]
-        profile.data["errors"] = profile.data["errors"][bs_indexes]
+        profile.data = self.data[bs_indexes]
 
-        profile.calculate_unscaled_profile_cached = lru_cache(5)(
-            profile.calculate_unscaled_profile
-        )
+        profile._calculate_unscaled_profile.cache_clear()
 
         return profile
 

chemex/experiments/cpmg/cpmg_profile.py
@@ -16,7 +16,10 @@ class CPMGProfile(base_profile.BaseProfile):
         super().__init__(name, data, exp_details, model)
 
         self.plot_data = plotting.plot_data
-        self.reference = self.data["ncycs"] == 0
+
+    @property
+    def reference(self):
+        return self.data["ncycs"] == 0
 
     def print_profile(self, params=None):
         """Print the CPMG profile."""
@@ -64,22 +67,11 @@ class CPMGProfile(base_profile.BaseProfile):
 
         self.mask = np.ones_like(self.data["intensities"], dtype=np.bool)
 
-    def make_mc_profile(self, params):
-        """Make a CPMG profile for boostrap analysis."""
-
-        profile = copy.copy(self)
-        profile.data["intensities"] = (
-            self.calculate_profile(params)
-            + np.random.randn(len(self.data["intensities"])) * self.data["errors"]
-        )
-
-        return profile
-
     def make_bs_profile(self):
         """Make a CPMG profile for boostrap analysis."""
         indexes = np.array(range(len(self.data["intensities"])))
         pool1 = indexes[self.reference]
-        pool2 = indexes[np.logical_not(self.reference)]
+        pool2 = indexes[~self.reference]
 
         bs_indexes = []
         if pool1.size:
@@ -89,12 +81,9 @@ class CPMGProfile(base_profile.BaseProfile):
         bs_indexes = sorted(bs_indexes)
 
         profile = copy.copy(self)
-        profile.data["ncycs"] = profile.data["ncycs"][bs_indexes]
-        profile.data["intensities"] = profile.data["intensities"][bs_indexes]
-        profile.data["errors"] = profile.data["errors"][bs_indexes]
-
-        profile.calculate_unscaled_profile_cached = lru_cache(5)(
-            profile.calculate_unscaled_profile
+        profile.data = self.data[bs_indexes]
+        profile.calculate_unscaled_profile = lru_cache(256)(
+            profile._calculate_unscaled_profile
         )
 
         return profile