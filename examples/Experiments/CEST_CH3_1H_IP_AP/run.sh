b37b7ba921258f248ef937d9622d10862d3ff82e
Guillaume Bouvignies <gbouvignies@gmail.com>
2019-09-02 18:20:11 +0200

    Add cest_1hn_ip_ap and cest_ch3_1h_ip_ap experiments

---
+++ examples/Experiments/CEST_CH3_1H_IP_AP/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+for i in 14 16 18 35 36 43 51 54 57 60
+do
+  chemex fit -e Experiments/*.toml \
+             -p Parameters/global.toml \
+                Parameters/cs_a.toml \
+                Parameters/dw_ab.toml \
+             -m Methods/method.toml \
+             -o Output/"$i"H-C \
+             +r "$i"H-C
+done
