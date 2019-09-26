baa2355d3f2e31478c12ebd6438bbc784e5311ba
Tairan Yuwen <tyuwen@pound.med.utoronto.ca>
2019-09-25 23:44:22 -0400

    Certain CEST example files are modified:
    1. Data file names in "CEST_13C" modified.
    
    2. Files for "fast" fitting method in "CEST_15N" added.
    
    3. The case for certain words in configuration files updated to make
    everything more consistent.

--- examples/Experiments/CEST_15N/run-fast.sh
+++ examples/Experiments/CEST_15N/run-fast.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-chemex fit -e Experiments/*hz.toml \
+chemex fit -e Experiments/*hz_fast.toml \
            -p Parameters/global.toml \
               Parameters/n15_cs.toml \
               Parameters/n15_dw.toml \
            -m Methods/method.toml \
-           -o Output
+           -o Output/Fast
