dee57fde7e8f9fd61090b9debd3a7a5da20719ac
Tairan Yuwen <tyuwen@purcell.chem.purdue.edu>
2020-04-01 18:21:04 +0800

    DCEST_15N_3States example updated
    DCEST_15N_3States example slightly updated, just make the file organization similar to other examples without really changing anything.

--- examples/Experiments/DCEST_15N_3States/run_FIFU_drd.sh
+++ examples/Experiments/DCEST_15N_3States/run_FIFU_drd.sh
@@ -3,9 +3,12 @@
 SELECTION="19 20 26 32 35 39"
 
 # Correct FIFU model, including DRD-CEST datasets
-chemex fit -e Experiments_DRD/*.toml \
-           -p Parameters_FIFU/*.toml \
-           -m Methods_FIFU/*.toml \
+chemex fit -e Experiments/*.toml \
+              Experiments/DRD/*.toml \
+           -p Parameters/cs_a.toml \
+              Parameters/dw_*.toml \
+              Parameters/global_FIFU.toml \
+           -m Methods/method_FIFU.toml \
            -d 3st \
            --include $SELECTION \
            -o Output_FIFU_DRD
