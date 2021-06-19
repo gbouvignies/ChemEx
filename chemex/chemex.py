aaf1c74b00736c65ea30ce1f8c921c3b6628d844
Guillaume Bouvignies <gbouvignies@gmail.com>
2021-06-19 23:39:11 +0200

    Enhance 'simulate' mode
    This commit follows Alex's suggestion to write the Data directory when
    using the 'simulate' command.

--- chemex/chemex.py
+++ chemex/chemex.py
@@ -4,9 +4,9 @@ import sys
 import chemex
 import chemex.cli as cc
 import chemex.containers.experiment as cce
-import chemex.containers.plot as ccp
 import chemex.helper as ch
 import chemex.optimize.fitting as cf
+import chemex.optimize.helper as coh
 import chemex.parameters.helper as cph
 import chemex.parameters.kinetics as cpk
 import chemex.parameters.settings as cps
@@ -62,8 +62,15 @@ def fit(args):
     # Filter datapoints out if necessary (e.g., on-resonance filter CEST)
     experiments.filter(params)
 
-    # Run the fit
+    if args.commands == "simulate":
+        _run_sim(args, experiments, params)
+    else:
+        _run_fit(args, defaults, experiments, params)
+
+
+def _run_fit(args, defaults, experiments, params):
     ch.header1("Running the main fit")
+
     fitter = cf.Fit(experiments, args.out_dir, args.plot, defaults)
     fitter.read_methods(args.method)
     params_fitted = fitter.run_methods(params)
@@ -75,35 +82,15 @@ def fit(args):
         fitter.mc_simulations(params_fitted, n_iter, mc_name)
 
 
-def simulate(args):
+def _run_sim(args, experiments, params):
 
-    # Parse kinetics model
-    model = cpk.parse_model(name=args.model)
+    ch.header1("Running the simulation")
 
-    # Read initial values of fitting/fixed parameters
-    defaults = cps.read_defaults(args.parameters)
-
-    # Read experimental setup and data
-    selection = {"include": args.include, "exclude": args.exclude}
-    experiments = cce.read(args.experiments, model, selection, defaults)
-
-    if not experiments:
-        sys.exit("\nerror: No data to simulate")
-
-    # Create parameters
-    params = cph.create_params(experiments, defaults)
-
-    # Set to "fix" all parameters that are set to "fit"
     for param in params.values():
         if param.vary:
             param.vary = False
 
-    # Filter datapoints out if necessary (e.g., on-resonance filter CEST)
-    experiments.filter(params)
-
-    # Run the simulation
-    ch.header1("Running the simulation")
     path = args.out_dir
-    print(f'Writing results -> "{path}/"\n')
-    cps.write_par(params, path)
-    ccp.write_plots(experiments, params, path, simulation=True)
+    plot = args.plot == "normal"
+
+    coh.post_fit(experiments, params, path, plot=plot, simulation=True)
