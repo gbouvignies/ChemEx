98ce2525d74e985bc37bb1af49046339606bcb1c
Guillaume Bouvignies <gbouvignies@gmail.com>
2023-04-19 00:32:47 +0200

    Correct for wrong use of private variables

--- chemex/models/model.py
+++ chemex/models/model.py
@@ -8,10 +8,10 @@ from chemex.models.factory import model_factory
 
 @dataclass
 class _Model:
-    __name: str = "2st"
-    __states: str = "ab"
-    __model_free: bool = False
-    __temp_coef: bool = False
+    _name: str = "2st"
+    _states: str = "ab"
+    _model_free: bool = False
+    _temp_coef: bool = False
 
     @staticmethod
     def validate_model_name(name: str) -> str:
@@ -25,27 +25,27 @@ class _Model:
 
     def set_model(self, name: str) -> None:
         kinetic_model_name, *ext = name.split(".")
-        self.__name = self.validate_model_name(kinetic_model_name)
+        self._name = self.validate_model_name(kinetic_model_name)
         state_nb = int(kinetic_model_name[0])
-        self.__states = ascii_lowercase[:state_nb]
-        self.__model_free = "mf" in ext if ext else False
-        self.__temp_coef = "tc" in ext if ext else False
+        self._states = ascii_lowercase[:state_nb]
+        self._model_free = "mf" in ext if ext else False
+        self._temp_coef = "tc" in ext if ext else False
 
     @property
     def name(self) -> str:
-        return self.__name
+        return self._name
 
     @property
     def states(self) -> str:
-        return self.__states
+        return self._states
 
     @property
     def model_free(self) -> bool:
-        return self.__model_free
+        return self._model_free
 
     @property
     def temp_coef(self) -> bool:
-        return self.__temp_coef
+        return self._temp_coef
 
 
 model = _Model()
