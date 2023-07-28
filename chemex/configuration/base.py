ca1325504dd747ff895e54758518dd832dc056cb
Guillaume Bouvignies <gbouvignies@gmail.com>
2023-07-28 11:59:48 +0200

    Fix `path` wrongly converted to lower case

--- chemex/configuration/base.py
+++ chemex/configuration/base.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Any
 
-from pydantic import BaseModel, ConfigDict, model_validator
+from pydantic import BaseModel, model_validator
 
 
 def ensure_list(variable: Any | list[Any] | None) -> list[Any]:
@@ -20,8 +20,6 @@ def to_lower(string: Any) -> Any:
 
 
 class BaseModelLowerCase(BaseModel):
-    model_config = ConfigDict(str_to_lower=True)
-
     @model_validator(mode="before")
     def key_to_lower(cls, model: dict[str, Any]) -> dict[str, Any]:
         return {to_lower(k): v for k, v in model.items()}
